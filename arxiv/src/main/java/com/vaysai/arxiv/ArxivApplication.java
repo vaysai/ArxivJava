ff118b07fc926319435fd765a440203696cb8143
Erickarroyo18 <erickarroyo@ciencias.unam.mx>
2022-12-14 15:59:52 -0600

    [stage] Directory Arxiv added

---
+++ arxiv/src/main/java/com/vaysai/arxiv/ArxivApplication.java
@@ -0,0 +1,13 @@
+package com.vaysai.arxiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArxivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArxivApplication.class, args);
+	}
+
+}
---
+++ arxiv/src/test/java/com/vaysai/arxiv/ArxivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaysai.arxiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArxivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
